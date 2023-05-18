a3b15ee18bca4af9122780998ddd78c68157bb0f
SamridhChicmic <samridh.gupta@chicmic.co.in>
2023-05-18 11:22:00 +0530

    Bomb Throw Script Changes

--- assets/script/GroundItem/BombScript.ts
+++ assets/script/GroundItem/BombScript.ts
@@ -1,9 +1,12 @@
-import { _decorator, Component, Node } from "cc";
+import { _decorator, BoxCollider, Component, Node, RigidBody } from "cc";
 const { ccclass, property } = _decorator;
 
 @ccclass("BombScript")
 export class BombScript extends Component {
+  BombCollider: BoxCollider;
   start() {
+    this.BombCollider = this.node.getComponent(BoxCollider);
+
     setTimeout(() => {
       this.node.destroy();
     }, 5000);
